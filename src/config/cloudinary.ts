9f8b33b1350162d0817ccb1ad1637c949a923bd0
John Alvin Rivera <johnalvin2121@gmail.com>
2026-05-08 11:34:15 +0800

    remove unused thumbnail images and update video category data to use Cloudinary image URLs

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,11 +1,16 @@
 const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
-const CLOUD_BASE = `https://res.cloudinary.com/${CLOUD_NAME}/video/upload`
+const CLOUD_BASE = `https://res.cloudinary.com/${CLOUD_NAME}`
+const VIDEO_BASE = `${CLOUD_BASE}/video/upload`
+const IMAGE_BASE = `${CLOUD_BASE}/image/upload`
 
 const isMobile = window.innerWidth <= 768
 const VIDEO_PARAMS = isMobile ? "q_auto,f_auto,w_720" : "q_auto,f_auto,w_1280"
 
 export const cdnVideo = (publicId: string): string =>
-  `${CLOUD_BASE}/${VIDEO_PARAMS}/${publicId}.mp4`
+  `${VIDEO_BASE}/${VIDEO_PARAMS}/${publicId}.mp4`
 
 export const cdnAudio = (publicId: string): string =>
-  `${CLOUD_BASE}/${publicId}.mp3`
+  `${VIDEO_BASE}/${publicId}.mp3`
+
+export const cdnImage = (publicId: string): string =>
+  `${IMAGE_BASE}/q_auto,f_auto,w_400/${publicId}`
--- src/data/videoCategoryData.ts
+++ src/data/videoCategoryData.ts
@@ -1,21 +1,11 @@
 import { v4 as uuidv4 } from "uuid"
-import { cdnVideo } from "../config/cloudinary"
+import { cdnImage, cdnVideo } from "../config/cloudinary"
 
 import icon_sunshine from "../assets/icons/icon--sunshine.png"
 import icon_snow from "../assets/icons/icon--snow.png"
 import icon_rain from "../assets/icons/icon--rain.png"
 import icon_night_owl from "../assets/icons/icon--moon.png"
 
-import cat_sunshine_thumbnail from "../assets/thumbnail/cat_sunshine.png"
-import chilling_cat_owl_thumbnail from "../assets/thumbnail/chilling_cat_owl.png"
-import chilling_girl_owl_thumbnail from "../assets/thumbnail/chilling_girl_owl.png"
-import cozy_rain_thumbnail from "../assets/thumbnail/cozy_rain.png"
-import fireplace_snow_thumbnail from "../assets/thumbnail/fireplace_snow.png"
-import night_study_owl_thumbnail from "../assets/thumbnail/night_study_owl.png"
-import reading_owl_thumbnail from "../assets/thumbnail/reading_owl.png"
-import sunset_sunshine_thumbnail from "../assets/thumbnail/sunset_sunshine.png"
-import writing_rain_thumbnail from "../assets/thumbnail/writing_rain.png"
-import writing_snow_thumbnail from "../assets/thumbnail/writing_snow.png"
 
 import type { VideoCategory } from "../types/types"
 
@@ -26,8 +16,8 @@ const videoCategoryData: VideoCategory[] = [
     category: "Sunshine",
     icon: icon_sunshine,
     videos: [
-      { src: cdnVideo("cat_sunshine_joo8ky"),    thumbnail: cat_sunshine_thumbnail },
-      { src: cdnVideo("sunset_sunshine_lu56p6"), thumbnail: sunset_sunshine_thumbnail },
+      { src: cdnVideo("cat_sunshine_joo8ky"),    thumbnail: cdnImage("cat_sunshine_eff5uf.png") },
+      { src: cdnVideo("sunset_sunshine_lu56p6"), thumbnail: cdnImage("sunset_sunshine_gbbrsj.png") },
     ],
   },
   {
@@ -35,8 +25,8 @@ const videoCategoryData: VideoCategory[] = [
     category: "Snow",
     icon: icon_snow,
     videos: [
-      { src: cdnVideo("writing_snow_gx3kli"),   thumbnail: writing_snow_thumbnail },
-      { src: cdnVideo("fireplace_snow_utavkw"), thumbnail: fireplace_snow_thumbnail },
+      { src: cdnVideo("writing_snow_gx3kli"),   thumbnail: cdnImage("writing_snow_lnmzpz.png") },
+      { src: cdnVideo("fireplace_snow_utavkw"), thumbnail: cdnImage("fireplace_snow_iai4rr.png") },
     ],
   },
   {
@@ -44,8 +34,8 @@ const videoCategoryData: VideoCategory[] = [
     category: "Rain",
     icon: icon_rain,
     videos: [
-      { src: cdnVideo("cozy_rain_widqg0"),    thumbnail: cozy_rain_thumbnail },
-      { src: cdnVideo("writing_rain_qggdcz"), thumbnail: writing_rain_thumbnail },
+      { src: cdnVideo("cozy_rain_widqg0"),    thumbnail: cdnImage("cozy_rain_ug292a.png") },
+      { src: cdnVideo("writing_rain_qggdcz"), thumbnail: cdnImage("writing_rain_gnl4pr.png") },
     ],
   },
   {
@@ -53,10 +43,10 @@ const videoCategoryData: VideoCategory[] = [
     category: "Night Owl",
     icon: icon_night_owl,
     videos: [
-      { src: cdnVideo("night_study_owl_wk70ti"),   thumbnail: night_study_owl_thumbnail },
-      { src: cdnVideo("reading_owl_cdlpb4"),       thumbnail: reading_owl_thumbnail },
-      { src: cdnVideo("chilling_cat_owl_khybf5"),  thumbnail: chilling_cat_owl_thumbnail },
-      { src: cdnVideo("chilling_girl_owl_el2evy"), thumbnail: chilling_girl_owl_thumbnail },
+      { src: cdnVideo("night_study_owl_wk70ti"),   thumbnail: cdnImage("night_study_owl_wzoqez.png") },
+      { src: cdnVideo("reading_owl_cdlpb4"),       thumbnail: cdnImage("reading_owl_uomabq.png") },
+      { src: cdnVideo("chilling_cat_owl_khybf5"),  thumbnail: cdnImage("chilling_cat_owl_bfjqso.png") },
+      { src: cdnVideo("chilling_girl_owl_el2evy"), thumbnail: cdnImage("chilling_girl_owl_eboqeb.png") },
     ],
   },
 ]
